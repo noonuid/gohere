2f1ee468b7d206d4eafe234a9aab3ebe566390e9
morningnoon <toofartohear@qq.com>
2023-04-07 22:08:39 +0800

    edit problem 0046

--- leetcode/problem0046/permutations.go
+++ leetcode/problem0046/permutations.go
@@ -2,7 +2,7 @@ package problem0046
 
 func permute(nums []int) [][]int {
 	var result = make([][]int, 0)
-	backtracking(&result, []int{}, nums)
+	backtrack(&result, []int{}, nums)
 
 	return result
 }
@@ -11,7 +11,7 @@ func permute(nums []int) [][]int {
 // result 为最终的结果集
 // path 为求解过程中的不完全排列结果
 // unused 为排列过程中还未使用过的数
-func backtracking(result *[][]int, path, unused []int) {
+func backtrack(result *[][]int, path, unused []int) {
 	// 所有的数均以使用，一次完整的排列完成
 	if len(unused) == 0 {
 		// 为 path 开辟新的空间，断开与之前空间的引用
@@ -24,17 +24,14 @@ func backtracking(result *[][]int, path, unused []int) {
 	// 依次选择 nums 中的数放置在新的位置
 	for i := 0; i < len(unused); i++ {
 		// 选择 i 处的数作为新的位置的结果
-		var cur = unused[i]
-		path = append(path, cur)
+		path = append(path, unused[i])
 
 		// i 处的数已经使用，从 nums 中移除
-		unused = append(unused[:i], unused[i+1:]...)
-
+		unusedNew := append(append([]int{}, unused[:i]...), unused[i+1:]...)
 		// 继续完成后序位置的排列
-		backtracking(result, path, unused)
+		backtrack(result, path, unusedNew)
 
 		// 返回本层之后恢复本层的 path, unused
 		path = path[:len(path)-1]
-		unused = append(unused[:i], append([]int{cur}, unused[i:]...)...)
 	}
 }
--- leetcode/problem0046/permutations_test.go
+++ leetcode/problem0046/permutations_test.go
@@ -5,33 +5,37 @@ import (
 	"testing"
 )
 
-// 测试用例
-var testCases = []struct {
-	nums []int
-	want [][]int
-}{
-	{
-		nums: []int{1, 2, 3},
-		want: [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}},
-	},
-	{
-		nums: []int{0, 1},
-		want: [][]int{{0, 1}, {1, 0}},
-	},
-	{
-		nums: []int{1},
-		want: [][]int{{1}},
-	},
-}
+func testFramework(t *testing.T, testFunc func([]int) [][]int) {
+	// 测试用例
+	testCases := []struct {
+		nums []int
+		want [][]int
+	}{
+		{
+			nums: []int{1, 2, 3},
+			want: [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}},
+		},
+		{
+			nums: []int{0, 1},
+			want: [][]int{{0, 1}, {1, 0}},
+		},
+		{
+			nums: []int{1},
+			want: [][]int{{1}},
+		},
+	}
 
-func TestPermute(t *testing.T) {
 	for caseIndex, testCase := range testCases {
-		var got = permute(testCase.nums)
-
+		// 被测方法的返回值
+		got := testFunc(testCase.nums)
 		// 如果方法返回的实际值与期望值不相等，则输出错误对应的测试用例信息
 		if !reflect.DeepEqual(got, testCase.want) {
 			t.Errorf("\ncaseIndex: %d, testCase: %v\ngot: %v, want: %v",
 				caseIndex, testCase, got, testCase.want)
 		}
 	}
+}
+
+func TestPermute(t *testing.T) {
+	testFramework(t, permute)
 }
